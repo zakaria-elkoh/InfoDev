c7df61832d70e34d517e07b2843101ace876e613
Mohamed072005 <amine072005@gmail.com>
2024-09-08 21:13:36 +0100

    handel errors in creation articles part

--- router/article.router.js
+++ router/article.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const articleController = require('../controller/article.controller');
 
 router.get('/', articleController.getArticles);
-router.get('/profile', articleController.getDetailsPage);
+router.get('/profile', articleController.getProfilePage);
 router.post('/create-article', articleController.createArticle);
 
 module.exports = router;
