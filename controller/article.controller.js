159398dafd4c0afae9ee41858b0ea4401218d703
Mohamed072005 <amine072005@gmail.com>
2024-09-08 19:15:18 +0100

    commit the changes before pulling

--- controller/article.controller.js
+++ controller/article.controller.js
@@ -8,9 +8,38 @@ exports.getArticles = async (req, res) => {
                 attributes: ['username']
             }]
         });
-        res.render('blog', { articles });
+        res.render('layout/layout', {
+            articles,
+            title: 'home',
+            currentPage: 'home',
+            currentView: '../homePage'
+        });
     } catch (error) {
         console.error('Error fetching articles:', error);
         res.status(500).send('Internal Server Error');
     }
 };
+
+exports.getDetailsPage = (req, res) => {
+    res.render('layout/layout', {
+        title: 'profile',
+        currentPage: 'profile',
+        currentView: '../profilePage'
+    });
+}
+
+exports.createArticle = async (req, res) => {
+    const { title, content } = req.body;
+    try {
+        const response = await Article.create({
+            title,
+            content,
+            userId: 1
+        });
+        res.redirect('/profile?created=success');
+    } catch (error) {
+        console.error('Error creating article:', error);
+        res.status(500).send('Internal Server Error');
+        res.redirect('/profile?created=fail');
+    }
+}
--- router/article.router.js
+++ router/article.router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const articleController = require('../controller/article.controller');
 
-router.get('/blog', articleController.getArticles);
+router.get('/', articleController.getArticles);
+router.get('/profile', articleController.getDetailsPage);
+router.post('/create-article', articleController.createArticle);
 
 module.exports = router;
